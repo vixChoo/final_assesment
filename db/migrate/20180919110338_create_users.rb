5b40d27d92678079f9981db804372cbbfe98fb59
Vixtrum <vixtrumchoo@yahoo.com>
2018-09-24 11:48:20 +0800

    Create User

--- db/migrate/20180919110338_create_users.rb
+++ db/migrate/20180919110338_create_users.rb
@@ -1,31 +1,31 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
-      t.string :password_digest
-      t.string :email
-      t.string :name
-      t.string :avatar
-      t.string :location
-      t.string :url
-      t.string :provider
-      t.string :uid
-      t.timestamps
-    
-    end
-    add_index :users, :provider
-    add_index :users, :uid
-    add_index :users, [:provider, :uid], unique: true
-  end
-end
 # class CreateUsers < ActiveRecord::Migration[5.2]
 #   def change
 #     create_table :users do |t|
 #       t.string :password_digest
 #       t.string :email
 #       t.string :name
-
+#       t.string :avatar
+#       t.string :location
+#       t.string :url
+#       t.string :provider
+#       t.string :uid
 #       t.timestamps
+    
 #     end
+#     add_index :users, :provider
+#     add_index :users, :uid
+#     add_index :users, [:provider, :uid], unique: true
 #   end
 # end
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :password_digest
+      t.string :email
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
    
